91af6a44288e628130e7d6244ad4bfca0b84024c
2556aditya <adichaurasia95@gmail.com>
2025-09-04 00:56:44 +0530

    bitach

--- second.java
+++ second.java
@@ -1,5 +1,6 @@
 public class second {
     public static void main(String[] args) {
         System.out.println("Hello from the second class!");
+        System.out.println("This is another Java program.");
     }
 }
