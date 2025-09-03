080dd11d2fb328e5a324d60b16234e80c23d2519
2556aditya <adichaurasia95@gmail.com>
2025-09-04 01:04:50 +0530

    yo i gat it

---
+++ fourth.java
@@ -0,0 +1,6 @@
+public class fourth {
+    public static void main(String[] args) {
+        System.out.println("Hello from the fourth class!");
+        System.out.println("This is yet another Java program.");
+    }
+}
