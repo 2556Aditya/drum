19e4df04486d48e780b543f46189ab72d2233a48
2556aditya <adichaurasia95@gmail.com>
2025-09-04 22:31:48 +0530

    good

--- second.java
+++ second.java
@@ -1,6 +1,9 @@
 public class second {
     public static void main(String[] args) {
-        System.out.println("Hello from the second class!");
-        System.out.println("This is another Java program.");
+        System.out.println("Hello, World!");
+        System.out.println("This is the second Java file.");
+        System.out.println("Java programming is fun!");
+
+        System.out.println("Goodbye, World!");
     }
 }
