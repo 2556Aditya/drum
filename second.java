312565ae25ab1ff5602510500e2ca10fbb04b7bd
2556aditya <adichaurasia95@gmail.com>
2025-09-04 22:47:43 +0530

    first rule

--- second.java
+++ second.java
@@ -2,7 +2,9 @@ public class second {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is the second Java file.");
-        
+        System.out.println("Java programming is fun!");
+        System.out.println("Let's write some code.");
+        System.out.println("Goodbye from the second class!");
 
         
     }
