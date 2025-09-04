a9bc5d973b786410ac3e1ef189178acb61d7b655
2556aditya <adichaurasia95@gmail.com>
2025-09-04 22:34:53 +0530

    Fixed

--- second.java
+++ second.java
@@ -2,8 +2,8 @@ public class second {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is the second Java file.");
-        System.out.println("Java programming is fun!");
+        
 
-        System.out.println("Goodbye, World!");
+        
     }
 }
